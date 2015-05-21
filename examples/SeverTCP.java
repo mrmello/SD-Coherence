c4c2f40a66984f29177bd664f9fd198f39a0fe8a
Marcos Mello <mrmello92@hotmail.com>
2015-05-21 13:36:37 -0300

    Receiving a command from the client

--- examples/SeverTCP.java
+++ examples/SeverTCP.java
@@ -6,35 +6,55 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
 public class ServerTCP {
 
 	public static void main(String[] args){
-		
+
 		ServerSocket serverSocket;
 		try {
-			serverSocket = new ServerSocket(2006);
+			serverSocket = new ServerSocket(1972);
+			System.out.println("Waiting...");
 			Socket s = serverSocket.accept();
-			OutputStream oip = s.getOutputStream();	
-			ObjectOutputStream write = new ObjectOutputStream(oip);
-			write.writeObject("Message from server");
-			InputStream ip = s.getInputStream();
-			ObjectInputStream read = new ObjectInputStream(ip);
-			String message;
-			try {
-				message = (String)read.readObject();
+			System.out.println("Connected!");
+			int option = 1;
+			String message = "";
+			while(true){
+				InputStream ip = s.getInputStream();
+				ObjectInputStream read = new ObjectInputStream(ip);
+				try {
+					option = (Integer)read.readObject();
+					switch(option){
+					case 1:
+						message = "Select";
+					break;
+					case 2:
+						message = "Insert";
+					break;
+					case 3:
+						message = "Update";
+					break;
+					case 4:
+						message = "Delete";
+					break;
+					default:
+						message = "Please verify the option!";
+				}
+					//encerra a comunicação
+					if(option == 5) 
+						break;
+					
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				}
 				System.out.println(message);
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
+				OutputStream oip = s.getOutputStream();	
+				ObjectOutputStream write = new ObjectOutputStream(oip);
+				write.writeObject(message);
 			}
 			s.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
-		
 	}
 
 }
