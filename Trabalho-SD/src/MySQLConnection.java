fe0e218d428028986ead663be17e36380b9bd182
Marcos Mello <mrmello92@hotmail.com>
2015-05-23 17:12:34 -0300

    Connecting to PostgreSQL instead

--- Trabalho-SD/src/MySQLConnection.java
+++ Trabalho-SD/src/MySQLConnection.java
@@ -1,38 +1,27 @@
 import java.sql.*;
 
+public class DBConnection {
 
+    // Replace <UTORid> and <DB_PASSWORD> with your login credentials.
+    public static final String DRIVER   = "org.postgresql.Driver";
+    public static final String URL      = "jdbc:postgresql://localhost/demello7";
+    public static final String UID      = "demello7";
+    public static final String PASSWORD = "14178";
 
-public class MySQLConnection {
+    private Connection con = null;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-		try {
-		    System.out.println("Loading driver...");
-		    Class.forName("com.mysql.jdbc.Driver");
-		    System.out.println("Driver loaded!");
-		} catch (ClassNotFoundException e) {
-		    throw new RuntimeException("Cannot find the driver in the classpath!", e);
-		}
-		
-		String url = "jdbc:mysql://localhost:3306/javabase";
-		String username = "java";
-		String password = "d$7hF_r!9Y";
-		Connection connection = null;
-		try {
-		    System.out.println("Connecting database...");
-		    connection = DriverManager.getConnection(url, username, password);
-		    System.out.println("Database connected!");
-		} catch (SQLException e) {
-		    throw new RuntimeException("Cannot connect the database!", e);
-		} finally {
-		    System.out.println("Closing the connection.");
-		    if (connection != null) try { connection.close(); } catch (SQLException ignore) {}
-		}
-		
+    public DBConnection() {
+    }
 
-	}
-
-}
+    public void connect() {
+        try {
+            Class.forName(DRIVER);
+            this.con = DriverManager.getConnection(URL, UID, PASSWORD);
+        } catch(ClassNotFoundException e) {
+            System.err.println("ClassNotFoundException: " + e.getMessage());
+            this.con = null;
+        } catch(SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+            this.con = null;
+        }
+    }
